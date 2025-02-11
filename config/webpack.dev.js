20afda94163b772f30922bbb3fdaf2f7e7b0f79a
caibowei1992 <610211409@qq.com>
2025-02-11 09:44:39 +0800

    change webpack

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,20 +2,20 @@ const { default: merge } = require("webpack-merge"),
   base = require("./webpack.base.js");
 module.exports = merge(base, {
   mode: "development",
-  devtool: "source-map",
+  devtool: "eval-cheap-source-map",
   devServer: {
     client: {
-      progress: true,
+      progress: true, //在浏览器中以百分比显示编译进度
     },
     compress: true,
     hot: true,
     open: {
       app: {
-        name: "goole-chrome",
-        arguments: ["--incognito", "--new-window"],
+        name: "chrome", // 打开指定浏览器各操作系统值不相同
+        arguments: ["--incognito", "--new-window"], // 打开无痕模式
       },
     },
-    port: 8081, //监听端口
+    port: 8081,
     proxy: [],
   },
 });
