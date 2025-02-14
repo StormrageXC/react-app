7e29918bd1fd15b82c820701d40ebdd76af9653c
caibowei1992 <610211409@qq.com>
2025-02-14 17:24:07 +0800

    add config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Login from "./login";
-const App = () => {
+export default function RequestTracker(): React.ReactElement {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending(pending + 1);
+    await delay(3000);
+    setPending((pending) => pending - 1);
+    setCompleted(completed + 1);
+  }
+
   return (
     <>
-      <Login />
+      <h3>等待：{pending}</h3>
+      <h3>完成：{completed}</h3>
+      <button onClick={handleClick}>购买</button>
     </>
   );
-};
+}
 
-export default App;
+function delay(ms: number) {
+  return new Promise((resolve) => {
+    console.log("延迟中...");
+    setTimeout(resolve, ms);
+  });
+}
